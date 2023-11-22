4613dbad53c5b354a1a0220bf2aeb262a44f468f
Brian Kariuki <107594995+DevBrianKariuki@users.noreply.github.com>
2023-11-22 21:33:10 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#2D2D45",
+        navyBlue: "#383855",
+        orange: "#FD783C",
+        lightBlue: "#A8A8C7",
+        white: "#FFFFFF",
+        lightGray: "#BBBBBB",
+      },
+      fontFamily: {
+        inter: ["inter", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+}
